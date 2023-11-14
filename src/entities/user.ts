360f898c67a1b5818bdc71cd41a608ffe53d7eac
Lukas Veiga <lukas.veiga@hotmail.com>
2023-11-14 16:24:10 -0300

    add new param active for User entity and unsubscribe and isSubscribed to manage the user status

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -3,13 +3,23 @@ import { UserData } from "./user-data";
 export class User {
   public readonly name: string;
   public readonly email: string;
+  private active: boolean;
 
-  private constructor(name: string, email: string) {
+  private constructor(name: string, email: string, active: boolean) {
     this.name = name;
     this.email = email;
+    this.active = active;
   }
 
   static create(userData: UserData): User {
-    return new User(userData.name, userData.email);
+    return new User(userData.name, userData.email, true);
+  }
+
+  public unsubscribe(): void {
+    this.active = false;
+  }
+
+  public isSubscribed(): boolean {
+    return this.active;
   }
 }
