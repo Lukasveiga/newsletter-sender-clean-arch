ca872970ad07cacb74f98879859fc63276ddd079
Lukas Veiga <lukas.veiga@hotmail.com>
2023-11-15 15:43:17 -0300

    refactor NameValidation test

--- src/entities/user/utils/name-validator.spec.ts
+++ src/entities/user/utils/name-validator.spec.ts
@@ -3,11 +3,15 @@ import { NameValidation } from "./name-validator";
 
 describe("NameValidation", () => {
   test("Should throw if invalid name is provided", () => {
-    const invalidNameCases: string[] = [
-      "",
-      "a",
-      "dvuwqbzpmjvurdgmemadjraabawkwzmqahghtfchhijhqziq",
-    ];
+    const emptyName: string = "";
+    const shortName: string = "a";
+    let longName: string = "";
+
+    for (let i = 0; i <= 45; i++) {
+      longName += "a";
+    }
+
+    const invalidNameCases: string[] = [emptyName, shortName, longName];
     for (const invalidNameCase of invalidNameCases) {
       const error = () => {
         NameValidation.validate(invalidNameCase);
