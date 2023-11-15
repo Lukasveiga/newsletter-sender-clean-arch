ce3928b6be8aec07cdb12e962e5e1b76d81199ac
Lukas Veiga <lukas.veiga@hotmail.com>
2023-11-14 21:25:27 -0300

    refactoring UserRepository port interface

--- src/usecases/ports/user-repository.ts
+++ src/usecases/ports/user-repository.ts
@@ -1,8 +1,9 @@
+import { User } from "../../entities/user";
 import { UserData } from "../../entities/user-data";
 
 export interface UserRepository {
-  add: (user: UserData) => Promise<void>;
-  findUserByEmail: (email: string) => Promise<UserData>;
-  findAllActiveUsers: () => Promise<UserData[]>;
-  updateActiveStatus: () => Promise<void>;
+  add(user: UserData): Promise<void>;
+  findUserByEmail(email: string): Promise<User | null>;
+  findAllActiveUsers(): Promise<User[]>;
+  updateActiveStatus(email: string): Promise<void>;
 }
