2ea6663adfbda22ba47cf35027279cd38b2ad298
Lukas Veiga <lukas.veiga@hotmail.com>
2023-11-25 14:58:59 -0300

    Ensure SubscribeUserController returns status code 400 when name is not provided

--- src/presenters/controllers/subscribe-user-controller/subscribe-user-controller.spec.ts
+++ src/presenters/controllers/subscribe-user-controller/subscribe-user-controller.spec.ts
@@ -2,8 +2,10 @@ import { SubscribeUserController } from "./subscribe-user-controller";
 import { SubscribeUserOnNewsletterList } from "../../../usecases/subscribe-user-on-newsletter-list/subscribe-user-on-newsletter-list";
 import { InMemoryUserRepository } from "../../../usecases/in-memory-user-repository/in-memory-user-repository";
 import { User } from "../../../entities/user/user";
+import { HttpRequest } from "../ports/http";
 
-const makeSut = (userlist: User[]) => {
+const makeSut = () => {
+  const userlist: User[] = [];
   const inMemoryUserRepository = new InMemoryUserRepository(userlist);
   const subscribeUserOnNewsletterListSpy = new SubscribeUserOnNewsletterList(
     inMemoryUserRepository
@@ -13,5 +15,17 @@ const makeSut = (userlist: User[]) => {
 };
 
 describe("SubscribeUserController", () => {
-  test("Should return status code 400 when user name is not provided", async () => {});
+  test("Should return status code 400 when user name is not provided", async () => {
+    const httpRequest: HttpRequest = {
+      body: {
+        email: "any_email@emailc.com",
+      },
+    };
+
+    const { sut } = makeSut();
+
+    const httpRespose = await sut.subscribe(httpRequest);
+    expect(httpRespose.statusCode).toEqual(400);
+    expect(httpRespose.body).toEqual("name is required");
+  });
 });
