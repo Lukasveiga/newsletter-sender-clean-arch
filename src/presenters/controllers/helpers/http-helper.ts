5aab1c1986de5876870198b499c8f6185c72d416
Lukas Veiga <lukas.veiga@hotmail.com>
2023-11-25 16:50:48 -0300

    add http-helper notFound

--- src/presenters/controllers/helpers/http-helper.ts
+++ src/presenters/controllers/helpers/http-helper.ts
@@ -8,6 +8,10 @@ export const ok = (body: any): HttpResponse => {
   return { statusCode: 200, body };
 };
 
+export const notFound = (error: Error): HttpResponse => {
+  return { statusCode: 404, body: error.message };
+};
+
 export const serverError = (): HttpResponse => {
   return { statusCode: 500, body: "Internal Server Error" };
 };
