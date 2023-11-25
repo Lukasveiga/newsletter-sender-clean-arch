acbd0f45565c490b78900bb74f2d5f90a49b5bc8
Lukas Veiga <lukas.veiga@hotmail.com>
2023-11-25 17:25:19 -0300

    Ensure UnsubscribeUserController returns status code 200 when user is successfully unsubscribed

--- src/presenters/controllers/unsubscribe-user-controller.spec.ts
+++ src/presenters/controllers/unsubscribe-user-controller.spec.ts
@@ -3,15 +3,17 @@ import { UnsubscribeUserController } from "./unsubscribe-user-controller";
 import { UnsubscribeUserFromNewsletterList } from "../../usecases/unsubscribe-user-of-newsletter-list/unsubscribe-user-from-newsletter-list";
 import { User } from "../../entities/user/user";
 
+const userTest: User = User.create({ name: "test_name", email: "test_email@email.com" });
+
 const makeSut = () => {
-  const userList: User[] = [User.create({ name: "test_name", email: "test_email@email.com" })];
+  const userList: User[] = [userTest];
   const inMemoryUserRepository = new InMemoryUserRepository(userList);
   const unsubscribeUserFromNewsletterList = new UnsubscribeUserFromNewsletterList(
     inMemoryUserRepository
   );
   const sut = new UnsubscribeUserController(unsubscribeUserFromNewsletterList);
 
-  return { sut, unsubscribeUserFromNewsletterList };
+  return { sut, unsubscribeUserFromNewsletterList, inMemoryUserRepository };
 };
 
 describe("UnsubscribeUserController", () => {
@@ -59,5 +61,23 @@ describe("UnsubscribeUserController", () => {
     const httpResponse = await sut.unsubscribe(httpRequest);
     expect(httpResponse.statusCode).toEqual(500);
     expect(httpResponse.body).toEqual("Internal Server Error");
+  });
+
+  test("Should return status code 200 when user is successfully unsubscribed", async () => {
+    const httpRequest = {
+      body: {
+        email: userTest.email,
+      },
+    };
+
+    const { sut } = makeSut();
+
+    expect(userTest.isSubscribed()).toBeTruthy();
+
+    const httpResponse = await sut.unsubscribe(httpRequest);
+
+    expect(httpResponse.statusCode).toEqual(200);
+    expect(httpResponse.body.message).toEqual("User unsubscribed");
+    expect(userTest.isSubscribed()).toBeFalsy();
   });
 });
