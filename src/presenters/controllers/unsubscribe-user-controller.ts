d4d5f0068b4cf2994f1e3582ea27d93a73b4af9f
Lukas Veiga <lukas.veiga@hotmail.com>
2023-11-25 16:50:58 -0300

    created UnsubscribeUserController

---
+++ src/presenters/controllers/unsubscribe-user-controller.ts
@@ -0,0 +1,26 @@
+import { UsecaseError } from "../../usecases/errors/usecase-error";
+import { UnsubscribeUser } from "./../../usecases/unsubscribe-user-of-newsletter-list/unsubscribe-user";
+import { MissingParamError } from "./errors/missing-param-error";
+import { badRequest, notFound, ok, serverError } from "./helpers/http-helper";
+import { HttpRequest, HttpResponse } from "./ports/http";
+
+export class UnsubscribeUserController {
+  constructor(private unsubscribeUser: UnsubscribeUser) {}
+
+  async unsubscribe(httpRequest: HttpRequest): Promise<HttpResponse> {
+    if (!httpRequest.body.email) {
+      return badRequest(new MissingParamError("email"));
+    }
+
+    try {
+      await this.unsubscribeUser.unsubscribeUserFromNewsletterList(httpRequest.body.email);
+      return ok({ message: "User unsubscribed" });
+    } catch (error) {
+      if (error instanceof UsecaseError) {
+        return notFound(error);
+      }
+
+      return serverError();
+    }
+  }
+}
