fb332c0a888918692dc1d6437628d40ff2decfc3
Lukas Veiga <lukas.veiga@hotmail.com>
2023-11-26 08:47:41 -0300

    created SendNewsletterController

---
+++ src/presenters/controllers/send-newsletter-controller.spec.ts
@@ -0,0 +1,3 @@
+describe("SendNewsletterController", () => {
+  test("", () => {});
+});
---
+++ src/presenters/controllers/send-newsletter-controller.ts
@@ -0,0 +1,19 @@
+import { SendNewsletter } from "../../usecases/send-newsletter-to-subscribed-users/send-newsletter";
+import { ok, serverError } from "./helpers/http-helper";
+import { HttpRequest, HttpResponse } from "./ports/http";
+
+export class SendNewsletterController {
+  constructor(private sendNewsletter: SendNewsletter) {}
+
+  async send(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const path = __dirname + "/templates/newsletter.html";
+      await this.sendNewsletter.sendNewsletterToSubscribedUsers(path);
+      return ok("Newsletters sent successfully");
+    } catch (error) {
+      return serverError();
+    }
+  }
+}
+
+console.log(__dirname);
