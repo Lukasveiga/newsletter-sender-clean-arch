9167c1d4760992cd525561c38103195513218e6f
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-13 14:23:29 -0300

    Refactor HandlebarsHtmlCompilerService imports and interface method name

--- src/infra/html-compiler-service/handlebars-html-compiler-service.ts
+++ src/infra/html-compiler-service/handlebars-html-compiler-service.ts
@@ -1,10 +1,10 @@
 import { Context, HtmlCompiler } from "../../usecases/ports/html-compiler";
-import * as handlebars from "handlebars";
-import * as fs from "fs/promises";
+import handlebars from "handlebars";
+import fs from "fs/promises";
 import { HtmlCompilerError } from "../../usecases/errors/html-compiler-error";
 
 export class HandlebarsHtmlCompilerService implements HtmlCompiler {
-  async compile(path: string, context: Context): Promise<string> {
+  async compileHtml(path: string, context: Context): Promise<string> {
     try {
       const html = await fs.readFile(path);
       const compiler = handlebars.compile(html.toString());
