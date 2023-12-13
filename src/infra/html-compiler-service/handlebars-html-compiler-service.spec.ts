ea03d77dd35ef406edeab9eef1b4059104835813
Lukas Veiga <lukas.veiga@hotmail.com>
2023-12-13 14:26:35 -0300

    Ensure HandlebarsHtmlCompilerService throws if handlebars throw

--- src/infra/html-compiler-service/handlebars-html-compiler-service.spec.ts
+++ src/infra/html-compiler-service/handlebars-html-compiler-service.spec.ts
@@ -1,26 +1,24 @@
 import { HtmlCompilerError } from "../../usecases/errors/html-compiler-error";
 import { Context } from "../../usecases/ports/html-compiler";
 import { HandlebarsHtmlCompilerService } from "./handlebars-html-compiler-service";
+import fs from "fs/promises";
+import handlebars from "handlebars";
 
 const sut = new HandlebarsHtmlCompilerService();
 
-const path = "./test_path";
+const path = "/html-test.html";
 const context: Context = { username: "username_test" };
 
-const readFileMock = jest.fn().mockReturnValue("");
-jest.mock("fs", () => ({
-  readFile: readFileMock,
-}));
+jest.mock("fs/promises");
+jest.mock("handlebars");
 
 describe("HandlebarsHtmlCompilerService", () => {
   test("Should throw if handlebars throws", async () => {
-    jest.mock("handlebars", () => ({
-      compile: jest.fn().mockImplementation(() => {
-        throw new Error();
-      }),
-    }));
+    handlebars.compile = jest.fn().mockImplementation(() => {
+      throw new Error();
+    });
 
-    const promise = sut.compile(path, context);
+    const promise = sut.compileHtml(path, context);
     expect(promise).rejects.toThrow(new HtmlCompilerError());
   });
 });
