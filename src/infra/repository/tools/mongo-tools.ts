3e020285ad75f1e3eed9b47a32d57be7d25c7876
Lukas Veiga <lukas.veiga@hotmail.com>
2024-01-19 14:45:40 -0300

    feat: Add MongoDb tools to help the connection and test

---
+++ src/infra/repository/tools/mongo-tools.ts
@@ -0,0 +1,21 @@
+import { MongoClient, Collection } from "mongodb";
+
+export class MongoTools {
+  private static client: MongoClient;
+
+  static async connect(uri: string): Promise<void> {
+    this.client = await MongoClient.connect(uri);
+  }
+
+  static async disconnect(): Promise<void> {
+    this.client.close();
+  }
+
+  static getCollection(name: string): Collection {
+    return this.client.db().collection(name);
+  }
+
+  static clearCollection(name: string): void {
+    this.client.db().collection(name).deleteMany({});
+  }
+}
