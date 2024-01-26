cfbc3e03301f0bb9c0cf0f055c544bc42bf2050e
Lukas Veiga <lukas.veiga@hotmail.com>
2024-01-26 16:38:25 -0300

    feat: Created route file configuration

---
+++ src/main/routes/newsletter-route.ts
@@ -0,0 +1,38 @@
+import { Request, Response, Router } from "express";
+import {
+  makeSendNewsletterController,
+  makeSubscribeUserController,
+  makeUnsubscribeUserController,
+} from "../factories";
+import { HttpRequest, HttpResponse } from "../../presenters/controllers/ports/http";
+
+const route = Router();
+
+const subscribeUserController = makeSubscribeUserController();
+const usnsubscribeUserController = makeUnsubscribeUserController();
+const sendNewsletterController = makeSendNewsletterController();
+
+route.post("/subscribe", async (req: Request, res: Response) => {
+  const httpRequest: HttpRequest = {
+    body: req.body,
+  };
+
+  const httpResponse: HttpResponse = await subscribeUserController.subscribe(httpRequest);
+  res.sendStatus(httpResponse.statusCode).json(httpResponse.body);
+});
+
+route.post("/unsubscribe", async (req: Request, res: Response) => {
+  const httpRequest: HttpRequest = {
+    body: req.body,
+  };
+
+  const httpResponse: HttpResponse = await usnsubscribeUserController.unsubscribe(httpRequest);
+  res.sendStatus(httpResponse.statusCode).json(httpResponse.body);
+});
+
+route.post("/send", async (req: Request, res: Response) => {
+  const httpResponse: HttpResponse = await sendNewsletterController.send();
+  res.sendStatus(httpResponse.statusCode).json(httpResponse.body);
+});
+
+export { route as newsletterRoute };
