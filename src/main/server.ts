1a85f3ff65a776863c2d8fb080011ea5d1fa3439
Lukas Veiga <lukas.veiga@hotmail.com>
2024-01-27 13:00:24 -0300

    feat: Add logic so select mongo url propertie by environment

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,9 +3,13 @@ import app from "./config/app";
 import "dotenv/config";
 
 const PORT = process.env.PORT || 3001;
+const MONGO_URL =
+  (process.env.MONGO_URL as string) === "prod"
+    ? process.env.MONGO_URL_PROD
+    : process.env.MONGO_URL_DEV;
 
 (async () => {
-  await MongoTools.connect(process.env.MONGO_URL as string);
+  await MongoTools.connect(MONGO_URL as string);
   app.listen(PORT, () => {
     console.log(`Express server listening on ${PORT} - environment: ${process.env.NODE_ENV}`);
   });
