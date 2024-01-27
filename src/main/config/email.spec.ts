93672f3899039cceded29130341f822f398964c3
Lukas Veiga <lukas.veiga@hotmail.com>
2024-01-27 10:22:35 -0300

    feat: Ensure emailOptions provide a correct properties for the prod environment

--- src/main/config/email.spec.ts
+++ src/main/config/email.spec.ts
@@ -16,7 +16,7 @@ describe("Email Options Environment", () => {
     process.env = {
       NODE_ENV: "dev",
       EMAIL_HOST_DEV: "dev_host",
-      EMAIL_PORT_DEV: "1",
+      EMAIL_PORT_DEV: "2",
       EMAIL_USER_DEV: "dev_user",
       EMAIL_PASSWORD_DEV: "dev_pass",
     };
@@ -26,6 +26,23 @@ describe("Email Options Environment", () => {
       port: Number.parseInt(process.env.EMAIL_PORT_DEV as string),
       user: process.env.EMAIL_USER_DEV,
       pass: process.env.EMAIL_PASSWORD_DEV,
+    });
+  });
+
+  test("Should return emailOptions with prod env properties", () => {
+    process.env = {
+      NODE_ENV: "prod",
+      EMAIL_HOST: "prod_host",
+      EMAIL_PORT: "1",
+      EMAIL_USER: "prod_user",
+      EMAIL_PASSWORD: "prod_pass",
+    };
+
+    expect(getEmailOptions()).toEqual({
+      host: process.env.EMAIL_HOST,
+      port: Number.parseInt(process.env.EMAIL_PORT as string),
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
     });
   });
 });
